cf794611d74feb78db5b42eaf88762e1b4465556
shevaenot <105449970+shevaenot@users.noreply.github.com>
2022-07-06 22:50:52 +0300

    ds

--- src/main/java/shevchenko/config/WebConfig.java
+++ src/main/java/shevchenko/config/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/view/");
         templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
 
